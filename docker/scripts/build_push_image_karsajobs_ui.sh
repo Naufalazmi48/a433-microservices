4315e0049dfc38a154c3b05adb5c698bae24a8bc
Lalu Naufal Azmi <Naufalazmi020@gmail.com>
2023-06-25 09:22:04 +0800

    feat:  add build and push image script

---
+++ docker/scripts/build_push_image_karsajobs_ui.sh
@@ -0,0 +1,11 @@
+# Build docker image
+docker build -t ghcr.io/naufalazmi48/karsajobs-ui:latest .
+
+# Show images
+docker images
+
+# Login to docker github container registry
+echo $GITHUB_ACCESS_TOKEN | docker login ghcr.io -u Naufalazmi48 --password-stdin
+
+# Push to github packages
+docker push ghcr.io/naufalazmi48/karsajobs-ui:latest
